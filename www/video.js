d92d446f7a493f580bf0da7e5c7b2d72fb2a1abd
Justin Obney <justinobney@gmail.com>
2013-09-16 15:15:30 -0500

    trying to fix this thing

--- www/video.js
+++ www/video.js
@@ -2,7 +2,7 @@ cordova.define("cordova/plugin/videoplayer",
   function(require, exports, module) {
     var exec = require("cordova/exec");
     var VideoPlayer = function () {};
- 
+
     /**
      * Starts the video player intent
      *
@@ -11,11 +11,11 @@ cordova.define("cordova/plugin/videoplayer",
     VideoPlayer.prototype.play = function(url) {
         exec(null, null, "VideoPlayer", "playVideo", [url]);
     };
- 
+
     var videoPlayer = new VideoPlayer();
     module.exports = videoPlayer;
 });
- 
+
 if (!window.plugins) {
     window.plugins = {};
 }
